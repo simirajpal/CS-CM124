58d7abb220f16fc01af4713b4a1ab27b9b0ac6ab
simirajpal <34111485+simirajpal@users.noreply.github.com>
2018-06-02 20:41:24 -0700

    algorithm works, need to implement chunks

--- em.py
+++ em.py
@@ -5,18 +5,18 @@ Haplotype Phasing for Recently Admixed Populations
 
 """
 
-import phasing.py
+import phasing
 import numpy as np
 
 # freq_lst needs to be a dictionary
 def expectation_step(haplotypes, hap_pairs, freq_lst):
 	probabilities = []
 	for i in range(len(hap_pairs)):
-		prob = []
+		probability = []
 		for p in range(len(hap_pairs[i])):
-			f1 = freq_lst[hap_pairs[i][p][0]]
-			f2 = freq_lst[hap_pairs[i][p][1]]
-			probability.append(find_probabaility(f1, f2, len(hap_pairs[i])))
+			f1 = freq_lst[''.join(hap_pairs[i][p][0])]
+			f2 = freq_lst[''.join(hap_pairs[i][p][1])]
+			probability.append(find_probability(f1, f2, len(hap_pairs[i])))
 		probabilities.append(probability)
 	frequencies = {}	
 	for haplotype in haplotypes:
@@ -24,12 +24,12 @@ def expectation_step(haplotypes, hap_pairs, freq_lst):
 		for j in range(len(hap_pairs)):
 			for q in range(len(hap_pairs[j])):
 				if hap_pairs[j][q][0] == haplotype or hap_pairs[j][q][1] == haplotype:
-					probs.append(probability[j][q])
-		frequencies[haplotype] = find_frequency(probs, len(haplotypes))
+					probs.append(probabilities[j][q])
+		frequencies[''.join(haplotype)] = find_frequency(probs, len(haplotypes))
 	return frequencies, probabilities			
 
 def find_probability(freq1, freq2, numOfPairs):
-	return (freq1*freq2)/(numOfPairs(freq1+freq2))
+	return (freq1*freq2)/(numOfPairs*freq1*freq2)
 	
 def find_frequency(probs, numOfPossibleHaps):
 	return sum(probs)/numOfPossibleHaps
@@ -42,14 +42,16 @@ def maximization_step(probabilities, hap_pairs):
 	return correct_pairs
 
 def em(filename, runs):
-	haplotypes = lst_haplotypes((loadfile(filename)))
-	hap_pairs = remove_duplicates(haplotypes)
-	frequencies = {1/(len(haplotypes)) for haplotype in haplotypes}
-	for run in runs:
+	hap_pairs = phasing.lst_haplotypes((phasing.loadfile(filename)))
+	haplotypes = phasing.remove_duplicates(hap_pairs)
+	frequencies = {}
+	for haplotype in haplotypes:
+		frequencies[''.join(haplotype)] = 1/(len(haplotypes))
+	for run in range(runs):
 		frequencies, probabilities = expectation_step(haplotypes, hap_pairs, frequencies)
 		answer = maximization_step(probabilities, hap_pairs)
 	return answer
 	
 
-print(em("data/test.txt", 2))
+print(em("data/test.txt", 10))
 			
